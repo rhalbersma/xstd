08044f6f364788edb372868aa56bc04960b60ee7
rhalbersma <rhalbersma@gmail.com>
2014-09-24 22:38:56 +0200

    initial commit

---
+++ include/xstd/bitset.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <xstd/bitset/algorithm.hpp>
+#include <xstd/bitset/input_range.hpp>
+#include <xstd/bitset/bitset.hpp>
---
+++ test/src/bitset/intrinsic/builtin.cpp
@@ -0,0 +1,3 @@
+#include <xstd/bitset/intrinsic/builtin.hpp>       // ctznz, clznz, ctz, clz, bsfnz, bsrnz, popcount
+namespace lib = xstd::builtin;
+#include <bitset/intrinsic/test.hpp>
