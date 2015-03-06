a02b74580091dbfc86835ad76ef0b6ca233aa013
rhalbersma <rhalbersma@gmail.com>
2015-03-06 11:14:34 +0100

    refactor TTI type extraction

--- include/xstd/pp/tti/type.hpp
+++ include/xstd/pp/tti/type.hpp
@@ -4,17 +4,14 @@
 #include <xstd/type_traits.hpp>                 // type_is
 #include <type_traits>                          // conditional
 
-#define XSTD_PP_TTI_TYPE(NAME, DEFAULT)                 \
-                                                        \
-XSTD_PP_TTI_HAS_TYPE(NAME)                              \
-XSTD_PP_TTI_BOX_TYPE(NAME)                              \
-                                                        \
-template<class T>                                       \
-using NAME ## _t = typename std::conditional_t<         \
-        has_type_ ## NAME ## _v<T>,                     \
-        box_type_ ## NAME <T>,                          \
-        xstd::type_is<DEFAULT>                          \
->::type;                                                \
-                                                        \
-template<class T>                                       \
-constexpr auto NAME ## _v = NAME ## _t<T>::value;       \
+#define XSTD_PP_TTI_TYPE(NAME, DEFAULT)         \
+                                                \
+XSTD_PP_TTI_HAS_TYPE(NAME)                      \
+XSTD_PP_TTI_BOX_TYPE(NAME)                      \
+                                                \
+template<class T>                               \
+using NAME = typename std::conditional_t<       \
+        has_type_ ## NAME ## _v<T>,             \
+        box_type_ ## NAME <T>,                  \
+        xstd::type_is<DEFAULT>                  \
+>::type;                                        \
--- include/xstd/pp/tti/type_no_default.hpp
+++ include/xstd/pp/tti/type_no_default.hpp
@@ -4,18 +4,15 @@
 #include <xstd/pp/tti/detail/missing_type.hpp>  // XSTD_PP_TTI_MISSING_TYPE
 #include <type_traits>                          // conditional
 
-#define XSTD_PP_TTI_TYPE_NO_DEFAULT(NAME)               \
-                                                        \
-XSTD_PP_TTI_HAS_TYPE(NAME)                              \
-XSTD_PP_TTI_BOX_TYPE(NAME)                              \
-XSTD_PP_TTI_MISSING_TYPE(NAME)                          \
-                                                        \
-template<class T>                                       \
-using NAME ## _t = typename std::conditional_t<         \
-        has_type_ ## NAME ## _v<T>,                     \
-        box_type_ ## NAME <T>,                          \
-        missing_type_ ## NAME <T>                       \
->::type;                                                \
-                                                        \
-template<class T>                                       \
-constexpr auto NAME ## _v = NAME ## _t<T>::value;       \
+#define XSTD_PP_TTI_TYPE_NO_DEFAULT(NAME)       \
+                                                \
+XSTD_PP_TTI_HAS_TYPE(NAME)                      \
+XSTD_PP_TTI_BOX_TYPE(NAME)                      \
+XSTD_PP_TTI_MISSING_TYPE(NAME)                  \
+                                                \
+template<class T>                               \
+using NAME = typename std::conditional_t<       \
+        has_type_ ## NAME ## _v<T>,             \
+        box_type_ ## NAME <T>,                  \
+        missing_type_ ## NAME <T>               \
+>::type;                                        \
