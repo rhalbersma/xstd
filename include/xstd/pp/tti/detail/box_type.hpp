a971702aa8e3975bb3877980576ce9a150b00c30
Rein Halbersma <Rein Halbersma@localhost>
2015-04-18 00:41:06 +0200

    refactor type traits introspection (inspired by N4436)

--- include/xstd/pp/tti/detail/box_type.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define XSTD_PP_TTI_BOX_TYPE(NAME)      \
-                                        \
-template<class T>                       \
-struct box_type_ ## NAME                \
-{                                       \
-        using type = typename T::NAME;  \
-};                                      \
