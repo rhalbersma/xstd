9306d28c552614b9fa46d4e31ce8ae126387b05e
rhalbersma <rhalbersma@gmail.com>
2015-04-28 21:35:40 +0200

    refactor TTI type extraction

---
+++ include/xstd/pp/tti/typename.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <xstd/type_traits.hpp> // is_detected, is_detected_v, detected_or_t
+
+#define XSTD_PP_TTI_TYPENAME(NAME, DEFAULT)                                     \
+                                                                                \
+template<class T>                                                               \
+using typename_ ## NAME = typename T::NAME;                                     \
+                                                                                \
+template<class T>                                                               \
+using is_ ## NAME = xstd::is_detected<typename_ ## NAME, T>;                    \
+                                                                                \
+template<class T>                                                               \
+constexpr auto is_ ## NAME ## _v = xstd::is_detected_v<typename_ ## NAME, T>;   \
+                                                                                \
+template<class T>                                                               \
+using NAME ## _t = xstd::detected_or_t<DEFAULT, typename_ ## NAME, T>;          \
