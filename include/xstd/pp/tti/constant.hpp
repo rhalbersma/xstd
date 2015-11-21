de992d566b32d66e3985c2cc2c167d0c5f36b06b
Rein Halbersma <rhalbersma@gmail.com>
2015-11-21 21:12:47 +0100

    copy libstdc++ implementation of detection-idiom

--- include/xstd/pp/tti/constant.hpp
+++ include/xstd/pp/tti/constant.hpp
@@ -1,17 +1,11 @@
 #pragma once
-#include <xstd/type_traits.hpp> // is_detected, is_detected_v, detected_or_t
+#include <xstd/type_traits.hpp> // detected_or_t
 #include <type_traits>          // integral_constant, remove_const
 
 #define XSTD_PP_TTI_CONSTANT(NAME, DEFAULT)                                                                             \
                                                                                                                         \
 template<class T>                                                                                                       \
 using constant_ ## NAME = std::integral_constant<std::remove_const_t<decltype(T::NAME)>, T::NAME>;                      \
-                                                                                                                        \
-template<class T>                                                                                                       \
-using is_ ## NAME = xstd::is_detected<constant_ ## NAME, T>;                                                            \
-                                                                                                                        \
-template<class T>                                                                                                       \
-constexpr auto is_ ## NAME ## _v = xstd::is_detected_v<constant_ ## NAME, T>;                                           \
                                                                                                                         \
 template<class T>                                                                                                       \
 using NAME ## _t = xstd::detected_or_t<std::integral_constant<decltype(DEFAULT), DEFAULT>, constant_ ## NAME, T>;       \
