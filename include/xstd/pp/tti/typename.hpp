16bdf0235d2b1089069d51c200882d2518746abd
rhalbersma <rhalbersma@gmail.com>
2016-05-31 00:29:37 +0200

    name rules traits consistently with detection-idiom is_name_v to check for presence, name_or_t for SFINAE-friendly extraction

--- include/xstd/pp/tti/typename.hpp
+++ include/xstd/pp/tti/typename.hpp
@@ -1,17 +1,14 @@
 #pragma once
-#include <xstd/type_traits.hpp> // is_detected, is_detected_v, detected_or_t
+#include <xstd/type_traits.hpp> // is_detected_v, detected_or_t
 
-#define XSTD_PP_TTI_TYPENAME(NAME, DEFAULT)                                     \
-                                                                                \
-template<class T>                                                               \
-using typename_ ## NAME = typename T::NAME;                                     \
-                                                                                \
-template<class T>                                                               \
-using is_ ## NAME = xstd::is_detected<typename_ ## NAME, T>;                    \
-                                                                                \
-template<class T>                                                               \
-constexpr auto is_ ## NAME ## _v = xstd::is_detected_v<typename_ ## NAME, T>;   \
-                                                                                \
-template<class T>                                                               \
-using NAME ## _t = xstd::detected_or_t<DEFAULT, typename_ ## NAME, T>;          \
+#define XSTD_PP_TTI_TYPENAME(NAME, DEFAULT)                             \
+                                                                        \
+template<class T>                                                       \
+using NAME ## _t = typename T::NAME ## _type;                           \
+                                                                        \
+template<class T>                                                       \
+constexpr auto is_ ## NAME ## _v = xstd::is_detected_v<NAME ## _t, T>;  \
+                                                                        \
+template<class T>                                                       \
+using NAME ## _or_t = xstd::detected_or_t<DEFAULT, NAME ## _t, T>;      \
 
