9a7ccf9c6a526e88cb4b3c7ce2f156a3de794113
rhalbersma <rhalbersma@gmail.com>
2016-06-14 23:55:19 +0200

    move to boost.range

--- test/src/cstdlib.cpp
+++ test/src/cstdlib.cpp
@@ -1,6 +1,6 @@
 #include <xstd/cstdlib.hpp>             // truncated_div, floored_div, euclidean_div, div_t
+#include <boost/range/algorithm.hpp>    // equal
 #include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_CASE, BOOST_CHECK, BOOST_AUTO_TEST_SUITE_END
-#include <algorithm>                    // equal
 #include <utility>                      // pair
 #include <vector>                       // vector
 
@@ -37,21 +37,21 @@ BOOST_AUTO_TEST_CASE(TruncatedFlooredEuclideanDivision)
         };
 
         BOOST_CHECK(
-                std::equal(begin(input), end(input), begin(truncated_result), end(truncated_result),
+                boost::equal(input, truncated_result,
                         [](auto const& lhs, auto const& rhs) {
                         return truncated_div(lhs.first, lhs.second) == rhs;
                 })
         );
 
         BOOST_CHECK(
-                std::equal(begin(input), end(input), begin(floored_result), end(floored_result),
+                boost::equal(input, floored_result,
                         [](auto const& lhs, auto const& rhs) {
                         return floored_div(lhs.first, lhs.second) == rhs;
                 })
         );
 
         BOOST_CHECK(
-                std::equal(begin(input), end(input), begin(euclidean_result), end(euclidean_result),
+                boost::equal(input, euclidean_result,
                         [](auto const& lhs, auto const& rhs) {
                         return euclidean_div(lhs.first, lhs.second) == rhs;
                 })
