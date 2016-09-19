8495d968412584e7f5e6e2b3f557fd498d58e143
Rein Halbersma <Rein Halbersma@localhost>
2016-09-20 00:16:46 +0200

    simplify type traits introspection

--- include/xstd/pp/tti/constant.hpp
+++ include/xstd/pp/tti/constant.hpp
@@ -1,18 +1,14 @@
 #pragma once
-#include <xstd/type_traits.hpp> // is_detected_v, detected_or_t
+#include <xstd/type_traits.hpp> // detected_or_t
 #include <type_traits>          // integral_constant, remove_const
 
-#define XSTD_PP_TTI_CONSTANT(NAME, DEFAULT)                                                                             \
-                                                                                                                        \
-template<class T>                                                                                                       \
-using NAME ## _constant = std::integral_constant<std::remove_const_t<decltype(T::NAME)>, T::NAME>;                      \
-                                                                                                                        \
-template<class T>                                                                                                       \
-constexpr auto is_ ## NAME ## _v = xstd::is_detected_v<NAME ## _constant, T>;                                           \
-                                                                                                                        \
-template<class T>                                                                                                       \
-using NAME ## _or_t = xstd::detected_or_t<std::integral_constant<decltype(DEFAULT), DEFAULT>, NAME ## _constant, T>;    \
-                                                                                                                        \
-template<class T>                                                                                                       \
-constexpr auto NAME ## _or_v = NAME ## _or_t<T>::value;                                                                 \
+#define XSTD_PP_TTI_CONSTANT(NAME, DEFAULT)                                                             \
+                                                                                                        \
+template<class T>                                                                                       \
+using NAME ## _constant = std::integral_constant<std::remove_const_t<decltype(T::NAME)>, T::NAME>;      \
+                                                                                                        \
+using NAME ## _default = std::integral_constant<decltype(DEFAULT), DEFAULT>;                            \
+                                                                                                        \
+template<class T>                                                                                       \
+constexpr auto NAME ## _v = xstd::detected_or_t<NAME ## _default, NAME ## _constant, T>::value;         \
 
