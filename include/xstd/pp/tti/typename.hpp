9e08f863077514e8b817e8a37d0b6623ce0d5d46
rhalbersma <rhalbersma@gmail.com>
2016-09-20 00:16:46 +0200

    simplify type traits introspection

--- include/xstd/pp/tti/typename.hpp
+++ include/xstd/pp/tti/typename.hpp
@@ -1,14 +1,13 @@
 #pragma once
-#include <xstd/type_traits.hpp> // is_detected_v, detected_or_t
+#include <xstd/type_traits.hpp> // detected_or_t
 
-#define XSTD_PP_TTI_TYPENAME(NAME, DEFAULT)                             \
-                                                                        \
-template<class T>                                                       \
-using NAME ## _t = typename T::NAME ## _type;                           \
-                                                                        \
-template<class T>                                                       \
-constexpr auto is_ ## NAME ## _v = xstd::is_detected_v<NAME ## _t, T>;  \
-                                                                        \
-template<class T>                                                       \
-using NAME ## _or_t = xstd::detected_or_t<DEFAULT, NAME ## _t, T>;      \
+#define XSTD_PP_TTI_TYPENAME(NAME, DEFAULT)                                     \
+                                                                                \
+template<class T>                                                               \
+using NAME ## _typename = typename T::NAME ## _type;                            \
+                                                                                \
+using NAME ## _default = DEFAULT;                                               \
+                                                                                \
+template<class T>                                                               \
+using NAME ## _t = xstd::detected_or_t<NAME ## _default, NAME ## _typename, T>; \
 
