9ed79c6d1d95b4bfd67d0944c2e89ee61259a270
Rein Halbersma <rhalbersma@gmail.com>
2016-12-19 19:35:47 +0100

    add missing braces in to_underlying_type

--- include/xstd/type_traits.hpp
+++ include/xstd/type_traits.hpp
@@ -3,7 +3,7 @@
 
 namespace xstd {
 
-template<class Enumeration, std::enable_if_t<std::is_enum<Enumeration>>...>
+template<class Enumeration, std::enable_if_t<std::is_enum<Enumeration>{}>...>
 constexpr auto to_underlying_type(Enumeration e) noexcept
 {
         return static_cast<std::underlying_type_t<Enumeration>>(e);
