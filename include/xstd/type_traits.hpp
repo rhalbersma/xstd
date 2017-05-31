237c40b2d3df6b1f226adf15410256b336beeb8e
rhalbersma <rhalbersma@gmail.com>
2017-05-31 20:23:26 +0200

    add to_underlying_type overload for integral_constant

--- include/xstd/type_traits.hpp
+++ include/xstd/type_traits.hpp
@@ -1,12 +1,22 @@
 #pragma once
-#include <type_traits>  // enable_if, is_enum, underlying_type
+#include <type_traits>  // enable_if, is_enum, underlying_type, integral_constant
 
 namespace xstd {
 
-template<class Enumeration, std::enable_if_t<std::is_enum_v<Enumeration>>...>
+template<class Enumeration, std::enable_if_t<
+        std::is_enum_v<Enumeration>
+>...>
 constexpr auto to_underlying_type(Enumeration e) noexcept
 {
         return static_cast<std::underlying_type_t<Enumeration>>(e);
+}
+
+template<class Enumeration, Enumeration N, std::enable_if_t<
+        std::is_enum_v<Enumeration>
+>...>
+constexpr auto to_underlying_type(std::integral_constant<Enumeration, N>) noexcept
+{
+        return static_cast<std::underlying_type_t<Enumeration>>(N);
 }
 
 }       // namespace xstd
