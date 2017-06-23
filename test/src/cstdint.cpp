5de47018e0deef14886aea0cc095ec5243245c4d
rhalbersma <rhalbersma@gmail.com>
2017-06-23 21:34:01 +0200

    [ctest] add dummy unit tests to get better coverage info

---
+++ test/src/cstdint.cpp
@@ -0,0 +1,16 @@
+//          Copyright Rein Halbersma 2010-2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <xstd/cstdint.hpp>
+#include <boost/test/unit_test.hpp>     // BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_CASE, BOOST_CHECK_EQUAL, BOOST_AUTO_TEST_SUITE_END
+
+BOOST_AUTO_TEST_SUITE(CStdInt)
+
+BOOST_AUTO_TEST_CASE(Dummy)
+{
+        BOOST_CHECK_EQUAL(true, true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
