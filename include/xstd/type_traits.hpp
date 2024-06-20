4c79bb3c2a9accdacdfd575962aca3e52b75cc21
rhalbersma <rhalbersma@gmail.com>
2024-06-20 18:52:44 +0200

    Add missing include

--- include/xstd/type_traits.hpp
+++ include/xstd/type_traits.hpp
@@ -5,6 +5,7 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <compare>      // partial_ordering
 #include <type_traits>  // bool_constant, conditional_t, integral_constant
 
 namespace xstd {
