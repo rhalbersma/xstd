dd902d47f7cb8679708d2120e378b6e6b9cdaa6d
rhalbersma <rhalbersma@gmail.com>
2026-07-19 00:45:27 +0200

    Test xstd::aligned_size

--- test/src/utility.cpp
+++ test/src/utility.cpp
@@ -41,4 +41,14 @@ BOOST_AUTO_TEST_CASE(ToUnderlyingType)
         XSTD_CONSTEXPR_CHECK_EQUAL(to_underlying(e5_<e5{}>()), static_cast<unsigned>(0));
 }
 
+BOOST_AUTO_TEST_CASE(AlignedSize)
+{
+        XSTD_CONSTEXPR_CHECK_EQUAL(aligned_size( 0, 8),  0);
+        XSTD_CONSTEXPR_CHECK_EQUAL(aligned_size( 1, 8),  8);
+        XSTD_CONSTEXPR_CHECK_EQUAL(aligned_size( 8, 8),  8);
+        XSTD_CONSTEXPR_CHECK_EQUAL(aligned_size( 9, 8), 16);
+        XSTD_CONSTEXPR_CHECK_EQUAL(aligned_size(64, 8), 64);
+        XSTD_CONSTEXPR_CHECK_EQUAL(aligned_size(65, 8), 72);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
