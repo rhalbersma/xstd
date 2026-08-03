c5b3d63fdfae15a5ddbd88918bb2a46afb9fe1d2
rhalbersma <rhalbersma@gmail.com>
2026-08-03 13:56:18 +0200

    Consolidate 128-bit alias tests

--- test/src/cstdint.cpp
+++ test/src/cstdint.cpp
@@ -3,10 +3,13 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <xstd/concepts.hpp>        // signed_integral_like, unsigned_integral_like
 #include <xstd/cstdint.hpp>         // int128_t, uint128_t
 #include <xstd/test/constexpr.hpp>  // XSTD_CONSTEXPR_CHECK
+#include <xstd/type_traits.hpp>     // make_signed_like_t, make_unsigned_like_t
 #include <boost/test/unit_test.hpp> // BOOST_AUTO_TEST_CASE, BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END
 #include <limits>                   // numeric_limits
+#include <type_traits>              // is_same_v
 
 BOOST_AUTO_TEST_SUITE(Cstdint)
 
@@ -18,6 +21,10 @@ BOOST_AUTO_TEST_CASE(Int128)
         static_assert(not std::numeric_limits<xstd::uint128_t>::is_signed);
         static_assert(std::numeric_limits<xstd::int128_t>::digits == 127);
         static_assert(std::numeric_limits<xstd::uint128_t>::digits == 128);
+        static_assert(xstd::signed_integral_like<xstd::int128_t>);
+        static_assert(xstd::unsigned_integral_like<xstd::uint128_t>);
+        static_assert(std::is_same_v<xstd::make_signed_like_t<xstd::uint128_t>, xstd::int128_t>);
+        static_assert(std::is_same_v<xstd::make_unsigned_like_t<xstd::int128_t>, xstd::uint128_t>);
 
         XSTD_CONSTEXPR_CHECK(xstd::int128_t{-1} < xstd::int128_t{0});
         XSTD_CONSTEXPR_CHECK((xstd::uint128_t{1} << 127) > xstd::uint128_t{0});
