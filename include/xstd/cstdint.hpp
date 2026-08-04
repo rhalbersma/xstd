4f1b9b31e6e05274c8609d8b79bfd97f2857074c
rhalbersma <rhalbersma@gmail.com>
2026-08-04 14:40:52 +0200

    Fix MSVC detection and clang-tidy breakage (#135)

--- include/xstd/cstdint.hpp
+++ include/xstd/cstdint.hpp
@@ -6,43 +6,6 @@
 #ifndef XSTD_CSTDINT_HPP
 #define XSTD_CSTDINT_HPP
 
-#include <cstdint>
-
-// Prefer the Microsoft STL's integer classes; otherwise use the compiler's
-// 128-bit extension.
-#ifdef _MSVC_STL_VERSION
-
-#include <__msvc_int128.hpp>
-
-namespace xstd {
-
-using int128_t = std::_Signed128;
-using uint128_t = std::_Unsigned128;
-
-} // namespace xstd
-
-#elifdef __SIZEOF_INT128__
-
-#ifdef __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wpedantic"
-#endif
-
-namespace xstd {
-
-using int128_t = __int128;
-using uint128_t = unsigned __int128;
-
-} // namespace xstd
-
-#ifdef __GNUC__
-#pragma GCC diagnostic pop
-#endif
-
-#else
-
-#error "xstd::int128_t requires GCC/Clang __int128 or the Microsoft STL 128-bit integer classes"
-
-#endif
+#include <xstd/config/int128.hpp> // int128_t, uint128_t
 
 #endif // XSTD_CSTDINT_HPP
