29b0e8a0b6c74e36006766d1b9219c0e2ff04958
rhalbersma <rhalbersma@gmail.com>
2026-08-18 11:19:36 +0200

    Add direct sign trait test includes

--- test/src/cstdint/int128.cpp
+++ test/src/cstdint/int128.cpp
@@ -3,9 +3,11 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-#include <xstd/cstdint/int128.hpp>  // int128, uint128, make_signed_t, make_unsigned_t
-#include <boost/test/unit_test.hpp> // BOOST_AUTO_TEST_CASE, BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END
-#include <concepts>                 // same_as
+#include <xstd/cstdint/int128.hpp>            // int128, uint128
+#include <xstd/type_traits/make_signed.hpp>   // make_signed_t
+#include <xstd/type_traits/make_unsigned.hpp> // make_unsigned_t
+#include <boost/test/unit_test.hpp>           // BOOST_AUTO_TEST_CASE, BOOST_AUTO_TEST_SUITE, BOOST_AUTO_TEST_SUITE_END
+#include <concepts>                           // same_as
 
 BOOST_AUTO_TEST_SUITE(CstdintInt128)
 
